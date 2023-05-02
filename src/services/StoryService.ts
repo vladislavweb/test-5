0a432c119ccb1b0d9f24e6d5509fc94f11344d92
Vlad Ivanov <vivanovweb@gmail.com>
2023-05-02 10:17:25 +0300

    update story service

--- src/services/StoryService.ts
+++ src/services/StoryService.ts
@@ -2,18 +2,18 @@ import Axios from "axios";
 
 import { Story } from "types";
 
-const API_URL = "https://hacker-news.firebaseio.com/v0";
+import config from "../application.json";
 
 class StoryService {
   async getIDsOfNewStories() {
-    return Axios.get<number[]>(`${API_URL}/newstories.json`, {
+    return Axios.get<number[]>(`${config.HACKER_NEWS_URL}/newstories.json`, {
       headers: {
         "Content-Type": "application/json",
       },
     });
   }
   async getStory(id: number) {
-    return await Axios.get<Story>(`${API_URL}/item/${id}.json`, {
+    return await Axios.get<Story>(`${config.HACKER_NEWS_URL}/item/${id}.json`, {
       headers: {
         "Content-Type": "application/json",
       },
@@ -23,7 +23,9 @@ class StoryService {
     try {
       const requests = ids.map((id) => this.getStory(id));
 
-      return Axios.all(requests);
+      const stories = await Axios.all(requests);
+
+      return stories.map((response) => response.data);
     } catch {
       return undefined;
     }
